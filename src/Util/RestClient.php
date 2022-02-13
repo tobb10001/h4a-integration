b6fe06524a434fb495051288c1254efebc90bb23
Tobias Fischer <t7f2001@gmail.com>
2022-02-13 20:32:31 +0100

    add function RestClient::dispatch_request

--- src/Util/RestClient.php
+++ src/Util/RestClient.php
@@ -3,6 +3,7 @@
 namespace Tobb10001\H4aIntegration\Util;
 
 use Tobb10001\H4aIntegration\Exceptions\InvalidUrlException;
+use Tobb10001\H4aIntegration\Exceptions\UnsuccessfulRequestException;
 
 abstract class RestClient {
 
@@ -10,25 +11,47 @@ abstract class RestClient {
      * Takes the URL, that a browswer-visitor would use to display a league with
      * focus on a team on H4A, and converts it to a URL, that the can be used to
      * retrieve the actual JSON data.
-     * @param string $league_url
-     * @return string
      */
     static function convert_league_url(string $league_url): string {
 
-        if (substr_count($league_url, '?') != 1) {
+        if (substr_count($league_url, '?') != 1)
             throw new InvalidUrlException("Wrong number of question makrs '?', exactly one expected");
-        }
 
         /* @phpstan-ignore-next-line */
         $query = substr($league_url, strpos($league_url, '?'));
         parse_str($query, $params);
 
-        if (!array_key_exists("lId", $params) || !array_key_exists("tId", $params)) {
+        if (!array_key_exists("lId", $params) || !array_key_exists("tId", $params))
             throw new InvalidUrlException("Parameter 'lId' or 'tId' is missing.");
-        }
 
         return sprintf(
             "https://spo.handball4all.de/service/if_g_json.php?ca=0&cl=%s&cmd=ps&ct=%s",
             $params["lId"], $params["tId"]);
+    }
+
+    /**
+     * Dispatch a GET-Request using curl.
+     * This is basically a wrapper around curl with the most common options.
+     * It return s the response body as a string, ignoring everything else.
+     *
+     * Might get options to modify the curl request and/or error handling in the
+     * future.
+     */
+    static function dispatch_request(string $url): string {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+
+        if ($result === false) 
+            throw new UnsuccessfulRequestException("curl_exec returned false");
+
+        curl_close($ch);
+
+        // curl_exec would return bool without CURLOPT_RETURNTRANSFER set.
+        // @phpstan-ignore-next-line
+        return $result;
     }
 }
--- tests/Util/RestClientTest.php
+++ tests/Util/RestClientTest.php
@@ -1,11 +1,16 @@
 <?php
 
+namespace Tobb10001\H4aIntegration\Util;
+
+use phpmock\phpunit\PHPMock;
 use PHPUnit\Framework\TestCase;
 
 use Tobb10001\H4aIntegration\Exceptions\InvalidUrlException;
-use Tobb10001\H4aIntegration\Util\RestClient;
+use Tobb10001\H4aIntegration\Exceptions\UnsuccessfulRequestException;
 
-class UrlTest extends TestCase {
+class RestClientTest extends TestCase {
+
+    use PHPMock;
 
     function test_convert_league_url_valid() {
         $api_url = RestClient::convert_league_url(
@@ -33,5 +38,45 @@ class UrlTest extends TestCase {
         RestClient::convert_league_url(
             "https://www.handball4all.de/home/portal/pfalz/#/league?ogId=171&tId=123456"
         );
+    }
+
+    function test_dispath_request() {
+        $url = "dummy_url";
+        $output = "dummy_output";
+
+        $curl_init = $this->getFunctionMock(__NAMESPACE__, "curl_init");
+        $curl_init
+            ->expects($this->once())
+            ->willReturn(null);
+
+        $curl_setopt = $this->getFunctionMock(__NAMESPACE__, "curl_setopt");
+        $curl_setopt
+            ->expects($this->exactly(2))
+            ->withConsecutive(
+                [null, \CURLOPT_URL, $url],
+                [null, \CURLOPT_RETURNTRANSFER, true]);
+
+        $curl_exec = $this->getFunctionMock(__NAMESPACE__, "curl_exec");
+        $curl_exec
+            ->expects($this->once())
+            ->with(null)
+            ->willReturn($output);
+
+        $curl_close = $this->getFunctionMock(__NAMESPACE__, "curl_close");
+        $curl_close
+            ->expects($this->once())
+            ->with(null);
+
+        $this->assertEquals($output, RestClient::dispatch_request($url));
+    }
+
+    function test_dispatch_request_failed_curl_request() {
+        $curl_exec = $this->getFunctionMock(__NAMESPACE__, "curl_exec");
+        $curl_exec
+            ->expects($this->once())
+            ->willReturn(false);
+
+        $this->expectException(UnsuccessfulRequestException::class);
+        RestClient::dispatch_request("dummy_url"); 
     }
 }
