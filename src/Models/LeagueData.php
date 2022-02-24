2a7761df552bc26fc30e64711e66981c9c4ae651
Tobias Fischer <t7f2001@gmail.com>
2022-02-24 20:55:38 +0100

    split league data and league metadata

--- src/Models/LeagueData.php
+++ src/Models/LeagueData.php
@@ -6,23 +6,9 @@ namespace Tobb10001\H4aIntegration\Models;
  * Represents an API response as returned by the League-Endpoint.
  */
 class LeagueData {
-	/** @var string $name The name of the league. */
-	public string $name;
-	/** @var string $sname The short version of the name of the league. */
-	public string $sname;
-	public string $headline1;
-	public string $headline2;
-	/**
-	 * @var string $actualized Human readable (german) string telling the last
-	 * time the data was actualized.
-	 */
-	public string $actualized;
-	/**
-	 * @var string $repURL URL to prepend to Game::$sGID to construct the
-	 * report URL.
-	 */
-	public string $repURL;
-	public bool $scoreShownDataPerGame;
+
+	/** @var LeagueMetadata $metadata The league's metadata. */
+	public LeagueMetadata $metadata;
 
 	/** @var Table $table The table. */
 	public Table $table;
@@ -35,13 +21,8 @@ class LeagueData {
 	 * @param GameSchedule $games
 	 */
 	public function __construct(array $input, Table $table, GameSchedule $games) {
-		$this->name = $input["name"];
-		$this->sname = $input["sname"];
-		$this->headline1 = $input["headline1"];
-		$this->headline2 = $input["headline2"];
-		$this->actualized = $input["actualized"];
-		$this->repURL = $input["repURL"];
-		$this->scoreShownDataPerGame = $input["scoreShownDataPerGame"];
+		$this->metadata = new LeagueMetadata($input);
+
 		$this->table = $table;
 		$this->games = $games;
 	}
---
+++ src/Models/LeagueMetadata.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Tobb10001\H4aIntegration\Models;
+
+class LeagueMetadata {
+
+	/** @var string $name The name of the league. */
+	public string $name;
+	/** @var string $sname The short version of the name of the league. */
+	public string $sname;
+	public string $headline1;
+	public string $headline2;
+	/**
+	 * @var string $actualized Human readable (german) string telling the last
+	 * time the data was actualized.
+	 */
+	public string $actualized;
+	/**
+	 * @var string $repURL URL to prepend to Game::$sGID to construct the
+	 * report URL.
+	 */
+	public string $repURL;
+	public bool $scoreShownDataPerGame;
+
+	/**
+	 * @param array<mixed> $input
+	 */
+	public function __construct(array $input) {
+		$this->name = $input["name"];
+		$this->sname = $input["sname"];
+		$this->headline1 = $input["headline1"];
+		$this->headline2 = $input["headline2"];
+		$this->actualized = $input["actualized"];
+		$this->repURL = $input["repURL"];
+		$this->scoreShownDataPerGame = $input["scoreShownDataPerGame"];
+	}
+}
