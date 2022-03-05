27488ffb9c2610c9548a90e6ff683924b403fcb9
Tobias Fischer <t7f2001@gmail.com>
2022-03-05 15:51:44 +0100

    add Updater class

---
+++ src/Updater.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tobb10001\H4aIntegration;
+
+use PhpParser\Node\Stmt\TryCatch;
+use Tobb10001\H4aIntegration\Exceptions\UnsuccessfulRequestException;
+use Tobb10001\H4aIntegration\Models\LeagueData;
+use Tobb10001\H4aIntegration\Models\Team;
+use Tobb10001\H4aIntegration\Persistence\PersistenceInterface;
+use Tobb10001\H4aIntegration\Util\HttpClient;
+
+/**
+ * Update the the teams registered in the given persistence.
+ */
+class Updater
+{
+    private PersistenceInterface $pi;
+    private HttpClient $hc;
+
+    /**
+     * @param PersistenceInterface $pi
+     * @param HttpClient $hc If $hc === null, an HttpClient-object is
+     * instanciated.
+     */
+    public function __construct(PersistenceInterface $pi, ?HttpClient $hc = null)
+    {
+        $this->pi = $pi;
+        $this->hc = $hc ?? new HttpClient();
+    }
+
+    private function leagueDataFor(Team $team): LeagueData
+    {
+        /**
+         * $team->leagueUrl is checked to be non-null before call.
+         * @phpstan-ignore-next-line
+         */
+        $json = $this->hc->getJson($team->leagueUrl);
+        return LeagueData::fromJson($json);
+    }
+
+    private function updateTeam(Team $team): void
+    {
+        if (!is_null($team->leagueUrl)) {
+            try {
+                $leagueData = $this->leagueDataFor($team);
+                /**
+                 * $team->leagueUrl is checked to be non-null before call.
+                 * @phpstan-ignore-next-line
+                 */
+                $this->pi->replaceLeagueData($team->id, $leagueData);
+            } catch (UnsuccessfulRequestException) {
+                trigger_error(
+                    "Leauge for Team {$team->id} ({$team->internalName}) could"
+                    . " not be updated, due to a failed request. Ignoring."
+                );
+            }
+        }
+    }
+
+    public function update(): void
+    {
+        $teams = $this->pi->getTeams();
+
+        foreach ($teams as $team) {
+            if (is_null($team->id)) {
+                trigger_error(
+                    self::class . "->update() got a team without an ID:"
+                        . $team->internalName . "Ignoring the team and continuing.",
+                    E_USER_WARNING
+                );
+                continue;
+            }
+            $this->updateTeam($team);
+        }
+    }
+}
---
+++ tests/unit/UpdaterTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tobb10001\H4aIntegration;
+
+use PHPUnit\Framework\TestCase;
+use Tobb10001\H4aIntegration\Exceptions\UnsuccessfulRequestException;
+use Tobb10001\H4aIntegration\Models\LeagueData;
+use Tobb10001\H4aIntegration\Models\Team;
+use Tobb10001\H4aIntegration\Persistence\PersistenceInterface;
+use Tobb10001\H4aIntegration\Util\HttpClient;
+
+/**
+ * @covers Updater;
+ */
+class UpdaterTest extends TestCase
+{
+    private function getMockPersistence(array $teams): PersistenceInterface
+    {
+        $mock = $this->createMock(PersistenceInterface::class);
+        $mock->expects($this->once())
+                        ->method("getTeams")
+                        ->willReturn($teams);
+        return $mock;
+    }
+
+    public function testUpdate(): void
+    {
+        $mockPersistence = $this->getMockPersistence(
+            [
+                new Team([
+                    "id" => 1,
+                    "internalName" => "HSG Eckbachtal 2",
+                    "leagueUrl" => "someUrl"
+                ])
+            ]
+        );
+        $mockHttp = $this->createMock(HttpClient::class);
+
+        $mockHttp->expects($this->once())
+                 ->method("getJson")
+                 ->with("someUrl")
+                 ->willReturn(
+                     json_decode(
+                         file_get_contents(__DIR__ . "/../assets/league_response.json"),
+                         true
+                     )
+                 );
+
+        $mockPersistence->expects($this->once())
+                        ->method("replaceLeagueData")
+                        ->with(1, $this->isInstanceOf(LeagueData::class))
+                        ->willReturn(true);
+
+        $updater = new Updater($mockPersistence, $mockHttp);
+
+        $updater->update();
+    }
+
+    public function testUpdateIssuesNoticeOnUnsuccessfulRequest(): void
+    {
+        $mockPersistence = $this->getMockPersistence(
+            [
+                new Team([
+                    "id" => 1,
+                    "internalName" => "HSG Eckbachtal 2",
+                    "leagueUrl" => "someUrl"
+                ])
+            ]
+        );
+        $mockHttp = $this->createMock(HttpClient::class);
+
+        $mockHttp->expects($this->once())
+                 ->method("getJson")
+                 ->with("someUrl")
+                 ->will($this->throwException(new UnsuccessfulRequestException("")));
+
+        $updater = new Updater($mockPersistence, $mockHttp);
+
+        $this->expectNotice();
+
+        $updater->update();
+    }
+
+    public function testUpdateUnupdatableTeams(): void
+    {
+        $mockPersistence = $this->getMockPersistence(
+            [
+                new Team([
+                    "id" => 1,
+                    "internalName" => "HSG Eckbachtal 2",
+                ])
+            ]
+        );
+        $mockPersistence->expects($this->never())->method("replaceLeagueData");
+        $mockHttp = $this->createMock(HttpClient::class);
+        $mockHttp->expects($this->never())->method("getJson");
+
+        $updater = new Updater($mockPersistence, $mockHttp);
+
+        $updater->update();
+    }
+}
