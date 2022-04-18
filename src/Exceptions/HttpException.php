fff4fb1e28dbcc809686174a63b0a6f918474a6d
Tobias Fischer <t7f2001@gmail.com>
2022-04-18 19:33:06 +0200

    implement HttpClient::get

---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tobb10001\H4aIntegration\Exceptions;
+
+class HttpException extends Exception
+{
+}
--- src/Util/HttpClient.php
+++ src/Util/HttpClient.php
@@ -4,12 +4,48 @@ declare(strict_types=1);
 
 namespace Tobb10001\H4aIntegration\Util;
 
+use Tobb10001\H4aIntegration\Exceptions\HttpException;
+
 class HttpClient
 {
-    public function get(string $url): ?string
+    /**
+     * @param string $url
+     * @param array<int, mixed> $curl_opts Additional options to be set for
+     * the curl_handle.
+     * @throws HttpException
+     */
+    public function get(string $url, array $curl_opts = []): string
     {
-        // TODO
-        return "";
+        $ch = curl_init($url);
+
+        if ($ch === false) {
+            throw new HttpException("Could not initialize curl handle.");
+        }
+
+        $curl_opt_defaults = [
+            CURLOPT_POST => 0,
+            CURLOPT_RETURNTRANSFER => true,
+        ];
+
+        // since the options are numeric, I don't trust array_merge here.
+        foreach ($curl_opt_defaults as $key => $value) {
+            if (!array_key_exists($key, $curl_opts)) {
+                $curl_opts[$key] = $value;
+            }
+        }
+
+        curl_setopt_array($ch, $curl_opts);
+
+        $response = curl_exec($ch);
+
+        if (!is_string($response)) {
+            throw new HttpException("Error on curl request: " . curl_error($ch));
+        }
+
+        // omit curl_close, since it does nothing as per PHP 8.0.0
+        // https://www.php.net/manual/en/function.curl-close.php
+
+        return $response;
     }
 
     /**
@@ -26,7 +62,7 @@ class HttpClient
         int $flags = 0
     ): mixed {
         $response = $this->get($url);
-        if (is_null($response)) {
+        if (empty($response)) {
             return null;
         }
 
